046d2c4fd40505dadbda2509cccbbf4cda224f83
Felix Münscher <MAIL>
2020-02-22 22:01:35 +0100

    Adds image-view component

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {AlertsComponent} from './alerts/alerts.component';
+import { ImageViewComponent } from './image-view/image-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlertsComponent
+    AlertsComponent,
+    ImageViewComponent
   ],
   imports: [
     BrowserModule,
